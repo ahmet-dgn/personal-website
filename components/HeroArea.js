d15848cd0037c277507034945cb9dc405eb2f164
Ahmet Can Doğan <ahmetcandgn@gmail.com>
2024-07-25 16:55:50 +0300

    design bug fixed

--- components/HeroArea.js
+++ components/HeroArea.js
@@ -91,11 +91,11 @@ export default function HeroArea() {
             onClose={setMobileMenuOpen}
           >
             <div className="fixed inset-0 z-50" />
-            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-10 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between ">
                 <Link
                   href="/"
-                  className="-m-1.5 p-1.5 text-blue-800 border-blue-800 border-2 rounded-md"
+                  className="-m-1.5 p-1.5  text-blue-800 border-blue-800 border-2 rounded-md"
                 >
                   <span className="sr-only">Ahmet Can Doğan</span>
 
